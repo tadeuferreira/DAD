56bce93034dd51a6d4c01a3673752d2f8bf10218
Bh1993 <bhenriques1993@gmail.com>
2016-10-06 17:43:44 +0100

    g

--- game/js/main.js
+++ game/js/main.js
@@ -4,3 +4,32 @@
 
 // Implementation:
 
+(function(){
+
+$(document).ready(function() {
+    $.ajax({
+        url: "http://198.211.118.123:8080/board/easy"
+    }).then(function(data) {
+
+    	myvar = data;
+    	console.log(myvar)
+       
+       //for(var i = 0; i<$('.dad-row').length; i++){
+
+       	$( ".dad-row" ).each(function() {
+       		var row = this;
+
+       		for (var j = 0; j< row.children.length; j++) {
+       			//var cell = row.children[j];
+       			//var input = cell.children[0];
+       			
+       			$('.dad-cell input').val(1);
+       		//row.children.each(function() {
+       		//});
+       }
+     });
+    });
+});
+
+})();
+
