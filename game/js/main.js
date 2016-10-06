63bbb197f0ece7e9a2c375fc0e75de7298d30a3a
Tadeu <2140238@my.ipleiria.pt>
2016-10-06 21:47:38 +0100

    highlight implemented

--- game/js/main.js
+++ game/js/main.js
@@ -6,16 +6,46 @@
 
 var main = function(){
 
-  load_css();
-  load_grill();
+
+ $('#highlightButtons').children().click(function(event) {
+  var value = $(this).val();
+  highlightCells(value);
+  console.log('avavsas')
+});
+
+
+ // var btnCheck = getElementById('#btn-check');
+//var btnNew = getElementById('#btn-new');
+ // var selectMode = getElementById('#select-mode');
+
+ load_css();
+ load_grill();
 };
 
 
 function load_css () {
-  // body... 
+  $('.dad-cell input').each(function (index, value) { 
+
+    if($(this).val() != null && !$(this).hasClass('initial')){
+      $(this).addClass('with-value');
+    }
+  });
+};
+
+function highlightCells (val) {
+  $('.dad-cell input').each(function (index, value) { 
+    //Remove all previously highlitghted nummbers
+    $(this).removeClass('highlight');
+    
+    if($(this).val() === val ){
+      $(this).toggleClass('highlight');
+    }
+
+  });
 };
 
 function load_grill () {
+
  $.ajax({url: "http://198.211.118.123:8080/board/easy"}).then(function(data) {
    myvar = data;
    console.log(myvar)
@@ -23,14 +53,16 @@ function load_grill () {
         $( ".dad-row" ).each(function() {
           var row = this;
           for (var j = 0; j< row.children.length; j++) {
+            
             //var cell = row.children[j];
             //var input = cell.children[0];       
             $('.dad-cell input').val(1);
           //row.children.each(function() {
           //});
-         }
-       });
-       });
+
+        }
+      });
+      });
 };
 
 $(document).ready(main);
